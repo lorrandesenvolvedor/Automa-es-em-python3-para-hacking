4674d894b665bcb9d970257ee59cfa92c6e34ba4
Lorran C. S. <lorrandesenvolvedor@gmail.com>
2026-04-23 17:20:38 -0300

    Add files via upload

---
+++ 2faCode/keys.py
@@ -0,0 +1,6 @@
+def keyscode():
+    return {
+        "SUA_CHAVE_DE_ACESSO"
+    }
+    
+# aqui e onde você define a sua Key, e logo após e só inciar o script pelo arquivo principal e informa o código 2fa aonde e pedido. os códigos são gerados em um período de 30 segundos, porém são imprimidos a cada 15 segundos
